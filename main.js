14b3a85bfe18d37d8101fa8d4426182d448e7984
daniel301r <daniel.munns247@gmail.com>
2019-01-28 23:00:06 +0100

    more updates to functionality

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const keyboard = document.querySelectorAll(".button");
 const screenDisplay = document.querySelector(".screenNums");
 
-// have a state to say whether or not it is playing or whether it is the first time
-
 // ----- play around with the below code tomorrow ---- that's how you target shit!!
 
 // const key = e.target
@@ -21,7 +19,15 @@ let secondNum = false;
 let continuation = false;
 
 function addDisplayNumbers(num) {
-    if (!secondNum) {
+    if (continuation) {
+        if (firstNum.length <= 0) {
+            firstNum.push(num);
+            screenDisplay.innerHTML = num;
+        } else {
+            firstNum.push(num);
+            screenDisplay.innerHTML = firstNum.join("");
+        }   
+    } else if (!secondNum) {
         if (firstNum.length <= 0) {
             firstNum.push(num);
             screenDisplay.innerHTML = num;
@@ -39,23 +45,97 @@ function addDisplayNumbers(num) {
         }
     }
 }
+function determineState() {
+    if (firstNum.length > 0 && nextNum.length > 0) {
+        continuation = true;
+    } else if (!secondNum) {
+        secondNum = true;
+    } else {
+        secondNum = false;
+    }
+}
+
+function parseFirstNumber() {
+    firstNum = firstNum.join("");
+    firstNum = parseInt(firstNum, 10);
+}
+
+function parseBothNumbers() {
+    firstNum = firstNum.join("");
+    firstNum = parseInt(firstNum, 10);
+    nextNum = nextNum.join("");
+    nextNum = parseInt(nextNum, 10);
+}
+
+function calculate () {
+    if (continuation) {
+        if (firstNum.length === 0) {
+            screenDisplay.innerHTML = totalSum;
+        } else if (nextNum.length === 0) {
+            parseFirstNumber();
+            if (mathFunction === "add"){
+                totalSum += firstNum;
+            } else if (mathFunction === "subtract") {
+                totalSum -= firstNum;
+            } else if (mathFunction === "divide") {
+                totalSum /= firstNum;
+            } else if (mathFunction === "multiply"){
+                totalSum *= firstNum;
+            }
+            firstNum = [];
+            screenDisplay.innerHTML = totalSum;
+        } else {
+            parseBothNumbers();
+            if (mathFunction === "add"){
+                totalSum = firstNum + nextNum;
+            } else if (mathFunction === "subtract") {
+                totalSum = firstNum - nextNum;
+            } else if (mathFunction === "divide") {
+                totalSum = firstNum / nextNum;
+            } else if (mathFunction === "multiply"){
+                totalSum = firstNum * nextNum;
+            }
+            screenDisplay.innerHTML = totalSum;
+            firstNum = [];
+            nextNum = [];
+        }
+    }
+}
+
+function bkSpace (num) {
+    num.pop();
+    if (num.length === 0) {
+        screenDisplay.innerHTML = "0";
+    } else {
+        screenDisplay.innerHTML = num.join("");
+    }
+}
+
+function zero (num) {
+    if (num.length === 0) {
+        if (continuation) {
+            screenDisplay.innerHTML = totalSum;
+        } else {
+            screenDisplay.innerHTML = "0";
+        }
+    } else {
+        num.push("0");
+        screenDisplay.innerHTML = num.join("");
+    }
+}
 
 function buttonHit() {
 
     if (this.id === "zero"){
-        if (!secondNum) {
-            firstNum.push("0");
-            screenDisplay.innerHTML = "0";
-            // function to stop the 0 appearing if the number before is 0
+        if (!secondNum || continuation) {
+            zero(firstNum);
         } else {
-            nextNum.push("0");
-            screenDisplay.innerHTML = "0";
-        }
-        
+            zero(nextNum);
+        }      
     } else if (this.id === "one") {    
-        addDisplayNumbers("1");        
+        addDisplayNumbers("1");         
     } else if (this.id === "two") {
-        addDisplayNumbers("2");        
+        addDisplayNumbers("2");     
     } else if (this.id === "three") {
         addDisplayNumbers("3"); 
     } else if (this.id === "four") {
@@ -70,94 +150,87 @@ function buttonHit() {
         addDisplayNumbers("8"); 
     } else if (this.id === "nine") {
         addDisplayNumbers("9"); 
-    } else if (this.id === "clearScreen") {
-        
-        if (!secondNum) {
+    } else if (this.id === "bkSpace") {
+        if (continuation || !secondNum){
+            bkSpace(firstNum);
+        } else {
+            bkSpace(nextNum);
+        }
+    } else if (this.id === "clearScreen") {   
+        if (continuation) {
+            firstNum = [];
+        } else if (!secondNum) {
             firstNum = [];
         } else {
             nextNum = [];
         }
-
         screenDisplay.innerHTML = "0";
-               
-
     } else if (this.id === "clearAll") {
-        
-        console.log("clear all"); 
         totalSum = 0;
         firstNum = [];
         nextNum = [];
         screenDisplay.innerHTML = "0";
         secondNum = false;
+        continuation = false;
         mathFunction = "";
 
     } else if (this.id === "add") {
-
-        mathFuntion = this.id;
-
-        if (firstNum.length > 0 && nextNum.length > 0) {
-            continuation = true;
-            console.log(continuation);
-        }
-
-        if (!secondNum) {
-            secondNum = true;
-        } else {
-            secondNum = false;
-        }
+        
+        determineState();
+        calculate();      
+        mathFunction = this.id;
 
     } else if (this.id === "subtract") {
 
-
+        determineState();
+        calculate();
         mathFunction = this.id;
-
-        if (!secondNum) {
-            secondNum = true;
-        } else {
-            secondNum = false;
-        }
 
     } else if (this.id === "divide") {
         
+        determineState();
+        calculate();
         mathFunction = this.id;
-
-        if (!secondNum) {
-            secondNum = true;
-        } else {
-            secondNum = false;
-        }
 
     } else if (this.id === "multiply") {
         
+        determineState();       
+        calculate();
         mathFunction = this.id;
 
-        if (!secondNum) {
-            secondNum = true;
+    } else if (this.id === "equals") {
+        if (continuation) {
+            parseFirstNumber();
+            if (mathFunction === "add"){
+                totalSum += firstNum;
+            } else if (mathFunction === "subtract") {
+                totalSum -= firstNum;
+            } else if (mathFunction === "divide") {
+                totalSum /= firstNum;
+            } else if (mathFunction === "multiply"){
+                totalSum *= firstNum;
+            }
+            screenDisplay.innerHTML = totalSum;
+            firstNum = [];
+            continuation = true;
+            secondNum = false;
         } else {
+            parseBothNumbers();            
+            if (mathFunction === "add"){
+                totalSum = firstNum + nextNum;
+            } else if (mathFunction === "subtract") {
+                totalSum = firstNum - nextNum;
+            } else if (mathFunction === "divide") {
+                totalSum = firstNum / nextNum;
+            } else if (mathFunction === "multiply"){
+                totalSum = firstNum * nextNum;
+            }
+            screenDisplay.innerHTML = totalSum;
+            firstNum = [];
+            nextNum = [];
+            continuation = true;
             secondNum = false;
         }
-
-    } else if (this.id === "equals") {
-        
-        firstNum = firstNum.join("");
-        firstNum = parseInt(firstNum, 10);
-        nextNum = nextNum.join("");
-        nextNum = parseInt(nextNum, 10);
-
-        let result = ""
-        
-        if (mathFunction === "add"){
-            result = firstNum + nextNum;
-        } else if (mathFunction === "subtract") {
-            console.log(firstNum, nextNum);
-            result = firstNum - nextNum;
-        } else if (mathFunction === "divide") {
-            result = firstNum / nextNum;
-        } else if (mathFunction === "multiply"){
-            result = firstNum * nextNum;
-        }
-
-        screenDisplay.innerHTML = result;
     }
 }
 
